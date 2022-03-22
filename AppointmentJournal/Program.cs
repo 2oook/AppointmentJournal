bbb7c1905554bd0cc47599faa97f3f33b6727fb7
2oook <iblow82@gmail.com>
2022-03-22 14:04:29 +0400

    Some refactoring

--- AppointmentJournal/Program.cs
+++ AppointmentJournal/Program.cs
@@ -1,27 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AppointmentJournal;
 
-namespace AppointmentJournal
-{
-    public class Program
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.UseDefaultServiceProvider(options => options.ValidateScopes = false); // отключить проверку областей видимости для сервисов, поставляемых средством DI
-                });
-    }
-}
+        webBuilder.UseStartup<Startup>();
+        webBuilder.UseDefaultServiceProvider(options => options.ValidateScopes = false);
+    })
+    .Build()
+    .Run();
--- AppointmentJournal/Startup.cs
+++ AppointmentJournal/Startup.cs
@@ -1,81 +1,74 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AppointmentJournal.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Identity;
 using AppointmentJournal.AppReversedDatabase;
 
-namespace AppointmentJournal
+namespace AppointmentJournal;
+
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; }
+        Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddDbContext<AppointmentJournalContext>(options => 
+            options.UseSqlServer(Configuration["Data:AppointmentJournal:ConnectionString"]));
+
+        services.AddDbContext<AppIdentityDbContext>(options => 
+            options.UseSqlServer(Configuration["Data:AppointmentJournalIdentity:ConnectionString"]));
+
+        services.AddControllersWithViews();
+
+        services
+            .AddRazorPages()
+            .AddRazorRuntimeCompilation();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
+            app.UseDeveloperExceptionPage();
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices, Configuration).Wait();
+        AppIdentityDbContext.CreateRoles(app.ApplicationServices).Wait();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+            endpoints.MapControllerRoute(
+                name: null,
+                pattern: "{category}/Page{servicePage:int}",
+                defaults: new { controller = "Home", action = "List", servicePage = 1 });
 
-            services.AddDbContext<AppointmentJournalContext>(options => options.UseSqlServer(Configuration["Data:AppointmentJournal:ConnectionString"]));
+            endpoints.MapControllerRoute(
+                name: "empty",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["Data:AppointmentJournalIdentity:ConnectionString"]));
-
-            services.AddControllersWithViews();
-
-            // включить компил€цию razor - страниц во врем€ выполнени€
-            services.AddRazorPages().AddRazorRuntimeCompilation();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            // —оздать аккаунт администратора, если его нет
-            AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices, Configuration).Wait();
-
-            // создать роли, если они не определены
-            AppIdentityDbContext.CreateRoles(app.ApplicationServices).Wait();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: null,
-                    pattern: "{category}/Page{servicePage:int}",
-                    defaults: new { controller = "Home", action = "List", servicePage = 1 });
-
-                endpoints.MapControllerRoute(
-                    name: "empty",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
-                     name: "default",
-                     pattern: "{controller}/{action}/{id?}");
-            });
-        }
+            endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action}/{id?}");
+        });
     }
 }
