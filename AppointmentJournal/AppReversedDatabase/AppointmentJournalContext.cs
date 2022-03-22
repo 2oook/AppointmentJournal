706f390ed020d6d069531a982ea5fd53a0d0f222
2oook <iblow82@gmail.com>
2022-03-22 16:12:53 +0400

    Added database migrations

--- AppointmentJournal/AppReversedDatabase/AppointmentJournalContext.cs
+++ AppointmentJournal/AppReversedDatabase/AppointmentJournalContext.cs
@@ -28,8 +28,7 @@ namespace AppointmentJournal.AppReversedDatabase
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AppointmentJournal;Trusted_Connection=True;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=AppointmentJournal;User ID=sa;Password=Password10;MultipleActiveResultSets=true");
             }
         }
 
---
+++ AppointmentJournal/Migrations/AppointmentJournal/20220322115233_Initial.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppointmentJournal.Migrations.AppointmentJournal
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AddressValue = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProducerID = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServicesCategories",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServicesCategories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkDays",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProducerID = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkDays", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false),
+                    ProducerID = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    CategoryID = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Services_ServicesCategories",
+                        column: x => x.CategoryID,
+                        principalTable: "ServicesCategories",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Services_User_ProducerID",
+                        column: x => x.ProducerID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkDaysTimeSpans",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WorkDayID = table.Column<long>(type: "bigint", nullable: false),
+                    BeginTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ServiceID = table.Column<long>(type: "bigint", nullable: false),
+                    AddressID = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkDaysTimeSpans", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_WorkDaysTimeSpans_Addresses",
+                        column: x => x.AddressID,
+                        principalTable: "Addresses",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_WorkDaysTimeSpans_Services",
+                        column: x => x.ServiceID,
+                        principalTable: "Services",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkDaysTimeSpans_WorkDays",
+                        column: x => x.WorkDayID,
+                        principalTable: "WorkDays",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceID = table.Column<long>(type: "bigint", nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime", nullable: false),
+                    WorkDayTimeSpanID = table.Column<long>(type: "bigint", nullable: false),
+                    ConsumerID = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Services",
+                        column: x => x.ServiceID,
+                        principalTable: "Services",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Appointments_WorkDaysTimeSpans",
+                        column: x => x.WorkDayTimeSpanID,
+                        principalTable: "WorkDaysTimeSpans",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ServiceID",
+                table: "Appointments",
+                column: "ServiceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_WorkDayID",
+                table: "Appointments",
+                column: "WorkDayTimeSpanID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_CategoryID",
+                table: "Services",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_ProducerID",
+                table: "Services",
+                column: "ProducerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkDaysTimeSpans_AddressID",
+                table: "WorkDaysTimeSpans",
+                column: "AddressID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkDaysTimeSpans_ServiceID",
+                table: "WorkDaysTimeSpans",
+                column: "ServiceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkDaysTimeSpans_WorkDayID",
+                table: "WorkDaysTimeSpans",
+                column: "WorkDayID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "WorkDaysTimeSpans");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "WorkDays");
+
+            migrationBuilder.DropTable(
+                name: "ServicesCategories");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
